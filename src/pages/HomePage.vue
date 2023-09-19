1fd0f74e8d040f43ffa5bcc030d3d2a947bd4523
michelecanini <studio.michelecanini@gmail.com>
2023-09-19 14:11:12 +0200

    [FEAT] added folder pages end HomePage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>HomePage</h1>
+            </div>
+            <div class="col-12">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum corrupti, necessitatibus neque quasi aut placeat aperiam, saepe molestiae, sit voluptas quod doloremque minus quibusdam dignissimos labore. Totam vitae libero dolores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia incidunt cumque, dolorem a sunt, maxime magni commodi alias laudantium nam iusto, aliquid error ut deserunt! Quae laborum doloremque cumque beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id, repudiandae fugiat natus ipsum voluptatem maiores doloremque perferendis dicta repellat vitae, aliquid fugit, tempore odio. Harum iusto aliquam laudantium quas.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum corrupti, necessitatibus neque quasi aut placeat aperiam, saepe molestiae, sit voluptas quod doloremque minus quibusdam dignissimos labore. Totam vitae libero dolores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia incidunt cumque, dolorem a sunt, maxime magni commodi alias laudantium nam iusto, aliquid error ut deserunt! Quae laborum doloremque cumque beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id, repudiandae fugiat natus ipsum voluptatem maiores doloremque perferendis dicta repellat vitae, aliquid fugit, tempore odio. Harum iusto aliquam laudantium quas.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum corrupti, necessitatibus neque quasi aut placeat aperiam, saepe molestiae, sit voluptas quod doloremque minus quibusdam dignissimos labore. Totam vitae libero dolores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia incidunt cumque, dolorem a sunt, maxime magni commodi alias laudantium nam iusto, aliquid error ut deserunt! Quae laborum doloremque cumque beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id, repudiandae fugiat natus ipsum voluptatem maiores doloremque perferendis dicta repellat vitae, aliquid fugit, tempore odio. Harum iusto aliquam laudantium quas.
+            </div>
+        </div> 
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
