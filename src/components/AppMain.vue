ca251f201db561e44a5202e43d3cbd6c48c8d9d1
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-19 13:16:27 +0200

    Upload new structure for frontend

---
+++ src/components/AppMain.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'AppMain',
+    data(){
+        return{
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
+   
+}
+</script>
+ 
+ <template lang="">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">BoolPetCare</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+ </template>
+ 
+ <style lang="">
+    
+ </style>
