c85b91c14904822608385dd1d00a28ea0acad5ff
Anieb22 <anielloverdino@gmail.com>
2023-09-20 14:46:01 +0200

    definita Navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,65 +1,77 @@
 <script>
 export default {
-    name: 'AppHeader',
-    data() {
-        return {
-            isScrolled: false,
-        }
+  name: 'AppHeader',
+  data() {
+    return {
+      isScrolled: false,
+    }
+  },
+  created() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 0) {
+        this.isScrolled = true;
+      } else {
+        this.isScrolled = false;
+      }
     },
-    created() {
-        window.addEventListener('scroll', this.handleScroll);
-    },
-    methods: {
-        handleScroll() {
-            if (window.scrollY > 0) {
-                this.isScrolled = true;
-            } else {
-                this.isScrolled = false;
-            }
-        },
-    },
-    beforeDestroy() {
-        window.removeEventListener('scroll', this.handleScroll);
-    },
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
 }
 </script>
 
 <template lang="">
+  <!--NAVBAR-->
     <nav class="navbar navbar-expand-lg bg-body-transparent fixed-top" :class="{ 'sticky-navbar': isScrolled, 'scrolled': isScrolled }">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                          </a>
-                          <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                          </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                      </ul>
-                      <form class="d-flex" role="search">
-                          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                          <button class="btn btn-outline-success" type="submit">Search</button>
-                      </form>
-                    </div>
-                </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 d-flex justify-content-between align-items-center">
+            <div class="col-1 d-flex align-items-baseline justify-content-between">
+              <a href="#">
+                <img src="./img/Logo-BDoctor.png" class="logo" alt="">
+              </a>
+            </div>
+            <!--UTENTI-->
+            <ul class="d-flex flex-row my-auto justify-content-between col-5" id="utente">
+              <li>
+                <a href="#">
+                  <strong>Come funziona</strong>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <strong>Chi siamo</strong>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <strong>Trova un professionista</strong>
+                </a>
+              </li>
+            </ul>
+            <!--PROFESSIONISTI-->
+            <ul class="d-flex flex-row my-auto py-2" id="professionista">
+              <li class="pe-3">
+                Sei un professionista?
+              </li>
+              <li>
+                <a href="#" class="pe-3">
+                  <strong>Accedi</strong>
+                </a>
+              </li>
+              <li  class="pe-4">
+                <a href="#">
+                  <strong>Registrati</strong>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </nav>
     <div class="jumbotron-lg">
         
@@ -74,21 +86,41 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.brand-logo {
+  width: 200px;
+}
+
+li,
+li a {
+  list-style-type: none;
+  text-decoration: none;
+  color: #3058a6;
+}
+
+ul#professionista {
+  background-color: #b5dcc9;
+  border-radius: 50px;
+}
+
+.logo {
+  width: 100%;
+}
+
 .jumbotron-lg {
-    width: 100vw;
-    height: 600px;
-    background-image: url('./img/usman-yousaf-2o0DZVkRvYg-unsplash-large.png');
-    background-size: contain;
+  width: 100vw;
+  height: 600px;
+  background-image: url('./img/usman-yousaf-2o0DZVkRvYg-unsplash-large.png');
+  background-size: contain;
 }
 
 .sticky-navbar {
-    top: 0;
-    background-color: rgba(255, 255, 255, 0.9);
-    z-index: 100;
-    transition: background-color 0.3s;
+  top: 0;
+  background-color: #b5dcc9;
+  z-index: 100;
+  transition: background-color 1s;
 }
 
 .sticky-navbar.scrolled {
-    background-color: rgba(255, 255, 255, 1);
+  background-color: #a7c6e5;
 }
 </style>
